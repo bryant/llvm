f6c8d8660b0386526550057cf7d5dca4a799e688
bryant <bryant@users.noreply.github.com>
2016-11-29 04:05:47 -0500

    env: add lnt shortcuts [exclude]

--- test/env.sh
+++ test/env.sh
@@ -12,3 +12,10 @@ function run_all {
     cmd="opt $OPTFLAGS -{2} {1} -o=../results/{2}/{1} 2>../results/{2}/{1}.err"
     find . -iname '*.ll' | parallel $cmd :::: - ::: memcpyopt memcpyopt-mssa
 }
+
+function lnt_benchmark {
+    lntflags="--sandbox /tmp/BAR --cc clang --cxx clang++ --use-cmake=cmake \
+--use-lit=llvm-lit --test-suite ~/3rd/test-suite --cmake-cache ReleaseNoLTO \
+-j32"
+    lnt runtest test-suite $lntflags "$@"
+}
