8f6ac5022d4f573ca8e417c0a32265560c2befb7
bryant <bryant@users.noreply.github.com>
2016-12-23 13:23:51 -0500

    add component validation

---
+++ cmake/modules/LLVMBuild.cmake
@@ -0,0 +1,23 @@
+# Ensure that each component:
+# - has valid dependencies -- all deps should themselves be components,
+# - do not self-depend.
+# This is analogous to LLVMProjectInfo.validate_components in the old
+# utils/llvm-build.
+function(validate_component_deps components)
+  foreach(component ${components})
+    get_target_property(cdeps ${component} INTERFACE_LINK_LIBRARIES)
+    list(GET cdeps 0 first_dep)
+    if(NOT ${first_dep} STREQUAL "cdeps-NOTFOUND")
+      foreach(cdep ${cdeps})
+        if("${cdep}" STREQUAL "${component}")
+          message(SEND_ERROR "${component} cannot depend on itself.")
+        elseif(cdep MATCHES "^LLVM")
+          list(FIND components ${cdep} idx)
+          if(${idx} EQUAL -1)
+            message(SEND_ERROR "${component} depends on ${cdep}, which cannot be found.")
+          endif()
+        endif("${cdep}" STREQUAL "${component}")
+      endforeach()
+    endif()
+  endforeach(component)
+endfunction(validate_component_deps)
